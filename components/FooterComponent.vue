c647c328dfc0a72414090dda6c1894a9b3ea8f1f
Anastasia <a.shironosova@altsoft.spb.ru>
2020-11-26 19:21:04 +0300

    footer 90%

--- components/FooterComponent.vue
+++ components/FooterComponent.vue
@@ -1,5 +1,149 @@
 <template>
+<footer>
+  <b-row class="footer-main-row">
+    <b-container class="footer-main-cont">
+      <b-row class="footer-content-row">
+        <b-col cols="12" xl="3" class="footer-content-col">
+          <b-list-group class="footer-list">ОБ АРХИВЕ
+            <b-list-group class="footer-sub-list" title="Общие сведения">Общие сведения
+              <b-list-group-item class="footer-list-item"><b-link to="">История архива, Правовые документы</b-link></b-list-group-item>
+              <b-list-group-item class="footer-list-item"><b-link to="">Список муниципальных архивов Ульяновской области, Вакансии</b-link></b-list-group-item>
+            </b-list-group>
+            <b-list-group class="footer-sub-list">Деятельность
+              <b-list-group-item class="footer-list-item"><b-link to="">Рассекречивание</b-link></b-list-group-item>
+              <b-list-group-item class="footer-list-item"><b-link to="">Планы развития архивного дела</b-link></b-list-group-item>
+              <b-list-group-item class="footer-list-item"><b-link to="">Отчеты о выполнении плана развития архивного дела</b-link></b-list-group-item>
+              <b-list-group-item class="footer-list-item"><b-link to="">Антикоррупционная деятельность</b-link></b-list-group-item>
+              <b-list-group-item class="footer-list-item"><b-link to="">Источники комплектования</b-link></b-list-group-item>
+              <b-list-group-item class="footer-list-item"><b-link to="">Отчёт о проведении специальной оценки условий труда</b-link></b-list-group-item>
+              <b-list-group-item class="footer-list-item"><b-link to="">Планы мероприятий по охране труда</b-link></b-list-group-item>
+            </b-list-group>
+          </b-list-group>
 
+        </b-col>
+        <b-col cols="12" xl="3" class="footer-content-col">
+          <b-list-group class="footer-list">УСЛУГИ
+
+              <b-list-group-item class="footer-list-item"><b-link to="">Социально-правовые и тематические запросы</b-link></b-list-group-item>
+              <b-list-group-item class="footer-list-item"><b-link to="">Обслуживание пользователей в читальном зале</b-link></b-list-group-item>
+              <b-list-group-item class="footer-list-item"><b-link to="">Приём документов от организаций, являющихся источниками комплектования архива</b-link></b-list-group-item>
+              <b-list-group-item class="footer-list-item"><b-link to="">Составление информационных документов</b-link></b-list-group-item>
+              <b-list-group-item class="footer-list-item"><b-link to="">Проведение информационных мероприятий</b-link></b-list-group-item>
+              <b-list-group-item class="footer-list-item"><b-link to="">Предоставление документов организациям для экспонирования</b-link></b-list-group-item>
+              <b-list-group-item class="footer-list-item"><b-link to="">Копирование документов и печатных изданий</b-link></b-list-group-item>
+              <b-list-group-item class="footer-list-item"><b-link to="">Обеспечение сохранности и упорядочение документов организаций</b-link></b-list-group-item>
+          </b-list-group>
+
+        </b-col>
+        <b-col cols="12" xl="3" class="footer-content-col">
+          <b-list-group class="footer-list">ТЕМАТИЧЕСКИЕ БАЗЫ ДАННЫХ
+
+            <b-list-group-item class="footer-list-item"><b-link to="">Дворяне Симбирской губернии</b-link></b-list-group-item>
+            <b-list-group-item class="footer-list-item"><b-link to="">Ревизские сказки</b-link></b-list-group-item>
+            <b-list-group-item class="footer-list-item"><b-link to="">Симбирское купечество</b-link></b-list-group-item>
+            <b-list-group-item class="footer-list-item"><b-link to="">Клировые ведомости</b-link></b-list-group-item>
+            <b-list-group-item class="footer-list-item"><b-link to="">Фотокаталог</b-link></b-list-group-item>
+            <b-list-group-item class="footer-list-item"><b-link to="">Военнопленные</b-link></b-list-group-item>
+            <b-list-group-item class="footer-list-item"><b-link to="">Захоронения</b-link></b-list-group-item>
+          </b-list-group>
+          <b-list-group class="footer-list">ПУТЕВОДИТЕЛИ
+
+            <b-list-group-item class="footer-list-item"><b-link to="">Фонды досоветского периода</b-link></b-list-group-item>
+            <b-list-group-item class="footer-list-item"><b-link to="">Фонды советского периода</b-link></b-list-group-item>
+
+          </b-list-group>
+        </b-col>
+        <b-col cols="12" xl="3" class="footer-content-col">
+          <b-list-group class="footer-list">УКАЗАТЕЛИ
+            <b-list-group-item class="footer-list-item"><b-link to="">Именной</b-link></b-list-group-item>
+            <b-list-group-item class="footer-list-item"><b-link to="">Географический</b-link></b-list-group-item>
+          </b-list-group>
+          <b-row class="contact-block">
+            <b-row class="contact-title">КОНТАКТЫ</b-row>
+            <b-row class="contact-info">
+              <b-col cols="1" class="contact-image-col">
+                <b-img src="~/assets/svg/tel d.svg" class="contact-icon"></b-img>
+              </b-col>
+              <b-col cols="11" class="contact-text-col">
+                <b-row class="contact-text">Приемная (тел./факс): +7(8422) 73-56-03</b-row>
+                <b-row class="contact-text">Прием граждан (тел): +7(8422) 73-56-03</b-row>
+              </b-col>
+            </b-row>
+            <b-row class="contact-info">
+              <b-col cols="1" class="contact-image-col">
+                <b-img src="~/assets/svg/email d.svg" class="contact-icon"></b-img>
+              </b-col>
+              <b-col cols="11" class="contact-text-col">
+                <b-row class="contact-text">ogugauo@mail.ru</b-row>
+              </b-col>
+            </b-row>
+            <b-row class="contact-info">
+              <b-col cols="1" class="contact-image-col">
+                <b-img src="~/assets/svg/adress d.svg" class="contact-icon"></b-img>
+              </b-col>
+              <b-col cols="11" class="contact-text-col">
+                <b-row class="contact-text">432017, г. Ульяновск, ул. 12 Сентября, д. 7а</b-row>
+              </b-col>
+            </b-row>
+            <b-row class="contact-info">
+              <b-col cols="1" class="contact-image-col">
+                <b-img src="~/assets/svg/clock d.svg" class="contact-icon"></b-img>
+              </b-col>
+              <b-col cols="11" class="contact-text-col">
+                <b-row class="contact-text">ПН - СР: 9<sup>00</sup> - 16<sup>00</sup>, ЧТ: 12<sup>00</sup> - 19<sup>00</sup></b-row>
+                <b-row class="contact-text">
+                ПТ: 09<sup>00</sup> - 15<sup>00</sup>, СБ,ВС - выходные
+                </b-row>
+              </b-col>
+            </b-row>
+            <b-row class="social-nets">
+              <b-img src="~/assets/svg/facebook.svg" class="social-icon"></b-img>
+              <b-img src="~/assets/svg/livejournal.svg" class="social-icon"></b-img>
+              <b-img src="~/assets/svg/instagram.svg" class="social-icon"></b-img>
+              <b-img src="~/assets/svg/twitter.svg" class="social-icon"></b-img>
+              <b-img src="~/assets/svg/youtube.svg" class="social-icon"></b-img>
+            </b-row>
+          </b-row>
+
+        </b-col>
+
+      </b-row>
+      <b-row class="footer-banner-row">
+        <div class="banner-container">
+          <b-img class="banner-img"></b-img>
+        </div>
+        <div class="banner-container">
+          <b-img class="banner-img"></b-img>
+        </div>
+        <div class="banner-container">
+          <b-img class="banner-img"></b-img>
+        </div>
+        <div class="banner-container">
+          <b-img class="banner-img"></b-img>
+        </div>
+        <div class="banner-container">
+          <b-img class="banner-img"></b-img>
+        </div>
+        <div class="banner-container">
+          <b-img class="banner-img"></b-img>
+        </div>
+        <div class="banner-container">
+          <b-img class="banner-img"></b-img>
+        </div>
+        <div class="banner-container">
+          <b-img class="banner-img"></b-img>
+        </div>
+        <div class="banner-container">
+          <b-img class="banner-img"></b-img>
+        </div>
+
+      </b-row>
+      <b-row class="footer-logo-row">
+
+      </b-row>
+    </b-container>
+  </b-row>
+</footer>
 </template>
 
 <script>
@@ -8,6 +152,129 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+/* по умолчанию -  мобильная версия - 0 - 767 px*/
+@media (min-width: 0) {
+
+  .footer-main-row {
+    background-color: #272620;
+    background-image: url("~assets/img/Vector Smart Object.png");
+    background-position: right bottom;
+    background-repeat: no-repeat;
+
+    .footer-main-cont {
+      color: white;
+      font-size: 14px;
+
+      .footer-content-row {
+        margin-top: 50px;
+        margin-bottom: 50px;
+        .footer-content-col {
+          .footer-list {
+            font-weight: bold;
+            margin-bottom: 30px;
+            .footer-sub-list {
+              font-weight: normal;
+              padding-left: 15px;
+              margin-top: 15px;
+              .footer-list-item {
+                background: transparent;
+                border-radius: 0;
+                border: none;
+                a {
+                  color: white;
+                  &:hover {
+                    color: white;
+                  }
+                }
+              }
+            }
+            .footer-list-item {
+              background: transparent;
+              border-radius: 0;
+              border: none;
+              font-weight: normal;
+              a {
+                color: white;
+                &:hover {
+                  color: white;
+                }
+              }
+            }
+          }
+
+          .contact-block {
+            flex-direction: column;
+            .contact-title {
+              font-weight: bold;
+            }
+            .contact-info {
+              flex-direction: row;
+              flex-wrap: unset;
+              .contact-image-col {
+                padding: 0;
+                display: flex;
+                .contact-icon {
+                  width: 20px;
+                  height: 20px;
+                  margin-bottom: auto;
+                  fill: #ffffff;
+                }
+              }
+              .contact-text-col {
+                padding: 0 0 0 10px;
+                height: 100%;
+                .contact-text {
+                  padding-top: 5px;
+                  padding-bottom: 5px;
+                  sup {
+                    top: 0.5em;
+                  }
+                }
+              }
+            }
+            .social-nets {
+              .social-icon {
+                width: 30px;
+                margin-left: 15px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .footer-banner-row {
+    margin: 50px 15px;
+    justify-content: space-between;
+    .banner-container {
+      width: 140px;
+      height: 105px;
+      background-color: #2f2d27;
+      margin-bottom: 30px;
+    }
+  }
+  .footer-logo-row {
+    padding-top: 30px;
+    padding-bottom: 30px;
+  }
+}
+/* планшетная версия - 768 - 1023 px*/
+@media (min-width: 768px) {
+  .footer-banner-row {
+    margin: 50px 0;
+    justify-content: space-between;
+    .banner-container {
+      width: 140px;
+      height: 105px;
+      background-color: #2f2d27;
+      margin-bottom: 30px;
+    }
+  }
+}
+/* ПК версия -  от 1024 px*/
+@media (min-width: 1024px) {
+
+}
 
 </style>
