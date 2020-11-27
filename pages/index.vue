5f54b2d9037bb144ab77f5d3e2526af100d3509e
Anastasia Shironosova <a.shironosova@altsoft.spb.ru>
2020-11-27 12:07:28 +0300

    + 1/2 поиска

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,38 @@
 <template>
+<b-row class="main-row">
+  <b-container>
+  <b-row class="search-row">
+    <SearchTabs/>
+  </b-row>
+  <b-row class="pop-sections">
 
+  </b-row>
+
+
+</b-container>
+</b-row>
 </template>
 
 <script>
-export default {}
+import SearchTabs from "~/pages/SearchTabs";
+
+export default {
+  components: {SearchTabs}
+}
 </script>
 
-<style>
+<style lang="scss">
+/* по умолчанию -  мобильная версия - 0 - 767 px*/
+@media (min-width: 0) {
+  .main-row {
+    background-image: url("~assets/img/bg.png");
+  }
+}
+@media (min-width: 768px) {
 
+}
+@media (min-width: 1024px) {
+
+
+}
 </style>
